a097dca29b48fa7ca3dfb63b9bdc6002b1189a90
ivangor246 <ivangor246@gmail.com>
2025-11-12 18:18:56 +0300

    Create building model

---
+++ src/app/models/buildings.py
@@ -0,0 +1,23 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base, num_9_6, str_10, str_255
+
+if TYPE_CHECKING:
+    from .streets import Street
+
+
+class Building(Base):
+    __tablename__ = 'buildings'
+
+    street_id: Mapped[int] = mapped_column(ForeignKey('streets.id', ondelete='RESTRICT'))
+    number: Mapped[str_10]
+    detail: Mapped[str_255]
+    latitude: Mapped[num_9_6]
+    longitude: Mapped[num_9_6]
+
+    street: Mapped['Street'] = relationship(back_populates='buildings')
+
+    __table_args__ = (UniqueConstraint('number', 'street_id', name='number_street_uc'),)
--- src/app/models/streets.py
+++ src/app/models/streets.py
@@ -1,11 +1,12 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base, str_100
 
 if TYPE_CHECKING:
+    from .buildings import Building
     from .cities import City
 
 
@@ -16,3 +17,9 @@ class Street(Base):
     city_id: Mapped[int] = mapped_column(ForeignKey('cities.id', ondelete='RESTRICT'))
 
     city: Mapped['City'] = relationship(back_populates='streets')
+    buildings: Mapped[list['Building']] = relationship(
+        back_populates='street',
+        passive_deletes=True,
+    )
+
+    __table_args__ = (UniqueConstraint('name', 'city_id', name='name_city_uc'),)
