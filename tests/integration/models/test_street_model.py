afb62c5256cb205f573f45ee86ae67ff9e9e32b9
ivangor246 <ivangor246@gmail.com>
2025-11-14 11:10:26 +0300

    Test street model

---
+++ tests/integration/models/test_street_model.py
@@ -0,0 +1,16 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from tests.utils import create_city, create_street
+
+
+@pytest.mark.asyncio
+@pytest.mark.usefixtures('clean_db')
+class TestStreetModel:
+    async def test_name_city_constraint(self, pg_session: AsyncSession):
+        await create_city(pg_session, 'Moscow')
+        await create_street(pg_session, 1, 'Tverskaya')
+
+        with pytest.raises(IntegrityError):
+            await create_street(pg_session, 1, 'Tverskaya')
