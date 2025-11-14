f339df56fb1aaf4f1828a77bbfb37c93e700dbf4
ivangor246 <ivangor246@gmail.com>
2025-11-14 15:50:26 +0300

    Create activity model

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -1,3 +1,4 @@
+from .activities import Activity
 from .buildings import Building
 from .cities import City
 from .streets import Street
---
+++ src/app/models/activities.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base, str_100
+
+
+class Activity(Base):
+    __tablename__ = 'activities'
+
+    name: Mapped[str_100] = mapped_column(unique=True)
+    depth: Mapped[int] = mapped_column(default=0)
+    parent_id: Mapped[int | None] = mapped_column(ForeignKey('activities.id', ondelete='RESTRICT'))
+
+    parent: Mapped['Activity'] = relationship(
+        back_populates='children',
+        remote_side='Activity.id',
+    )
+    children: Mapped[list['Activity']] = relationship(
+        back_populates='parent',
+        passive_deletes=True,
+    )
